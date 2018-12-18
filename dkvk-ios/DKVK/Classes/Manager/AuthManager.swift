bbf9b45dbe75a0a626bdc29e94790bc37d6d433d
Rasl-Ait <aitekov_rasul@mail.ru>
2018-12-18 21:09:41 +0300

    add save user image database

--- dkvk-ios/DKVK/Classes/Manager/AuthManager.swift
+++ dkvk-ios/DKVK/Classes/Manager/AuthManager.swift
@@ -21,20 +21,61 @@ class AuthManager {
 	
 	private let auth = Auth.auth()
 	
-	func register(with model: RegisterModel, completion: VoidClosure) {
-		guard model.isFilled else {
-			return
-		}
-		guard let email = model.email, let password = model.password else {
-			return
-		}
+	func register(with model: RegisterModel, onSuccess: @escaping VoidClosure, onError: @escaping (_ errorMessage: String?) -> Void) {
+		guard model.isFilled else { return }
+		guard let email = model.email, let password = model.password	else { return }
+		guard let user = self.auth.currentUser else { return }
+		guard let imageData = model.photo?.jpegData(compressionQuality: 0.2) else { return }
 		
-		let usersRef = sourceRef.child("users")
-		let id = UUID.init().uuidString
 		auth.createUser(withEmail: email, password: password) { (result, error) in
-			var dict = model.dict
-			dict["id"] = id
-			usersRef.child(id).setValue(dict)
+			
+			if let error = error {
+				print(error.localizedDescription)
+			}
+			
+			let storageRef = Storage.storage().reference().child("profile_image").child(user.uid)
+			let metadata = StorageMetadata()
+			metadata.contentType = "image/jpg"
+
+			storageRef.putData(imageData, metadata: metadata) { metadata, error in
+				
+				if let error = error {
+					print(error.localizedDescription)
+					return
+				}
+			
+				else {
+					storageRef.downloadURL { url, error in
+						if let error = error {
+							print(error.localizedDescription)
+							return
+						}
+						
+						if url != nil {
+							
+							user.sendEmailVerification(completion: nil)
+							
+							var dict = model.dict
+							dict["photo"] = url?.absoluteString
+							self.registerUserIntoDatabase(uid: user.uid, dict: dict, onSuccess: onSuccess)
+							
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	private func registerUserIntoDatabase(uid: String, dict: [String : Any], onSuccess: @escaping VoidClosure) {
+		let userRef = self.sourceRef.child("users").child(uid)
+		userRef.setValue(dict) { error, ref in
+			if let error = error {
+				print(error.localizedDescription)
+				return
+			}
+			
+			onSuccess()
+			
 		}
 	}
 }
